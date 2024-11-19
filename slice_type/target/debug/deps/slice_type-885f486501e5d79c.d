e2a7de9a612a83d023c29ba4182cd2c84919443b
Ravi <rs1806105@gmail.com>
2024-11-19 17:05:43 +0530

    slice setup

---
+++ slice_type/target/debug/deps/slice_type-885f486501e5d79c.d
@@ -0,0 +1,5 @@
+C:\Users\ravi1\OneDrive\Desktop\rust\slice_type\target\debug\deps\libslice_type-885f486501e5d79c.rmeta: src/main.rs
+
+C:\Users\ravi1\OneDrive\Desktop\rust\slice_type\target\debug\deps\slice_type-885f486501e5d79c.d: src/main.rs
+
+src/main.rs:
---
+++ slice_type/target/debug/deps/slice_type-fe62524a3aae7ba1.d
@@ -0,0 +1,5 @@
+C:\Users\ravi1\OneDrive\Desktop\rust\slice_type\target\debug\deps\libslice_type-fe62524a3aae7ba1.rmeta: src/main.rs
+
+C:\Users\ravi1\OneDrive\Desktop\rust\slice_type\target\debug\deps\slice_type-fe62524a3aae7ba1.d: src/main.rs
+
+src/main.rs:
