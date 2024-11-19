da16df32355508fe2e24fc1a7002cbb634bb1124
Ravi <rs1806105@gmail.com>
2024-11-19 14:15:34 +0530

    reference borrowing setup

---
+++ references_borrowing/target/debug/deps/references_borrowing-2194a4a28e7912f9.d
@@ -0,0 +1,5 @@
+C:\Users\ravi1\OneDrive\Desktop\rust\references_borrowing\target\debug\deps\libreferences_borrowing-2194a4a28e7912f9.rmeta: src/main.rs
+
+C:\Users\ravi1\OneDrive\Desktop\rust\references_borrowing\target\debug\deps\references_borrowing-2194a4a28e7912f9.d: src/main.rs
+
+src/main.rs:
---
+++ references_borrowing/target/debug/deps/references_borrowing-8bfa7ff96d0151f1.d
@@ -0,0 +1,5 @@
+C:\Users\ravi1\OneDrive\Desktop\rust\references_borrowing\target\debug\deps\libreferences_borrowing-8bfa7ff96d0151f1.rmeta: src/main.rs
+
+C:\Users\ravi1\OneDrive\Desktop\rust\references_borrowing\target\debug\deps\references_borrowing-8bfa7ff96d0151f1.d: src/main.rs
+
+src/main.rs:
