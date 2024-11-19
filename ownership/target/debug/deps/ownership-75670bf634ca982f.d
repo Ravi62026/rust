e5d9e7ac4459fa07a53bfce6ad76c772a61fde11
Ravi <rs1806105@gmail.com>
2024-11-19 10:14:10 +0530

    ownership setup

---
+++ ownership/target/debug/deps/ownership-75670bf634ca982f.d
@@ -0,0 +1,5 @@
+C:\Users\ravi1\OneDrive\Desktop\rust\ownership\target\debug\deps\libownership-75670bf634ca982f.rmeta: src/main.rs
+
+C:\Users\ravi1\OneDrive\Desktop\rust\ownership\target\debug\deps\ownership-75670bf634ca982f.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/deps/ownership-7c2c0b28e0f95f25.d
@@ -0,0 +1,5 @@
+C:\Users\ravi1\OneDrive\Desktop\rust\ownership\target\debug\deps\libownership-7c2c0b28e0f95f25.rmeta: src/main.rs
+
+C:\Users\ravi1\OneDrive\Desktop\rust\ownership\target\debug\deps\ownership-7c2c0b28e0f95f25.d: src/main.rs
+
+src/main.rs:
